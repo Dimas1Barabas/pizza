ef190657eb0f2d93eb7895130321049862cea55c
ndmit <n.dmitriymih@gmail.com>
2026-06-16 04:54:14 +0300

    3.2

--- shared/components/shared/email-temapltes/verification-user.tsx
+++ shared/components/shared/email-temapltes/verification-user.tsx
@@ -11,7 +11,7 @@ export const VerificationUserTemplate: React.FC<Props> = ({ code }) => (
     </p>
 
     <p>
-      <a href={`http://localhost:3000/api/auth/verify?code=${code}`}>Подтвердить регистрацию</a>
+      <a href={`https://pizza2026.vercel.app/api/auth/verify?code=${code}`}>Подтвердить регистрацию</a>
     </p>
   </div>
 );
